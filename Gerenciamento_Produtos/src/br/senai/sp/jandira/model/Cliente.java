93fde47e2aa16293668f71751b0db6823bd22f9d
Vitoria <vazevedo73@gmail.com>
2023-11-29 17:13:08 -0300

    .

--- Gerenciamento_Produtos/src/br/senai/sp/jandira/model/Cliente.java
+++ Gerenciamento_Produtos/src/br/senai/sp/jandira/model/Cliente.java
@@ -1,4 +1,39 @@
 package br.senai.sp.jandira.model;
 
 public class Cliente {
+    private String nome;
+    private long cpf, telefone;
+    private int id_cliente;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(long telefone) {
+        this.telefone = telefone;
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
 }
--- Gerenciamento_Produtos/src/br/senai/sp/jandira/model/Conexao.java
+++ Gerenciamento_Produtos/src/br/senai/sp/jandira/model/Conexao.java
@@ -1,4 +1,30 @@
 package br.senai.sp.jandira.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+    private String servidor, banco, usuario, senha;
+    public Connection conexao;
+
+    public Conexao(){
+        this.servidor = "localhost";
+        this.banco = "loja_computadores";
+        this.usuario = "root";
+        this.senha = "bcd127";
+    }
+
+    public void ConnectDriver (){
+        try {
+            this.conexao = DriverManager.getConnection("jdbc:mysql://" + this.servidor + "/" + this.banco, this.usuario, this.senha);
+        } catch (SQLException erro) {
+            System.out.println(erro);
+        }
+    }
+
+    public Connection getConexao(){
+        ConnectDriver();
+        return conexao;
+    }
 }
